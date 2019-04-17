821fe8a5c76cdbed0718a75a753b27e018e614f4
shun takagi <s.takagi.910@gmail.com>
2019-04-18 01:38:43 +0900

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,4 @@
 User.create(email: 'test@test.com', password: 'testtest', nickname: 'shun', name: 'shun takagi', thx_balance: 1000, address: '123')
 User.create(email: 'test2@test.com', password: 'testtest2', nickname: 'tarou', name: 'tarou sasaki', thx_balance: 1000, address: '234')
 User.create(email: 'test3@test.com', password: 'testtest3', nickname: 'hazime', name: 'hazime kato', thx_balance: 1000, address: '345')
+User.all.each{|user| user.avatar.attach(io: File.open("#{File.join(Rails.root, "lib", "default_avatar.png")}"), filename: "default_avatar.png", content_type: "image/png")}
